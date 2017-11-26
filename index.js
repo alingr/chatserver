8d460bbdb1db364330cb255404f97f57490b1936
alingr <groza.alin88@gmail.com>
2017-11-26 11:36:43 -0500

    deploy

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+
+var http = require('http')
+, url = require('url')
+, fs = require('fs')
+, server;
+
+server = http.createServer(function(req, res){
+// parse the pathname as a url
+var path = url.parse(req.url).pathname;
+                   
+switch (path){
+    
+
+	
+	// serve up our html chat client file by writing it directly to the response
+	case '/':
+		fs.readFile(__dirname + '/chatclient2.html', function(err, data){
+	    	if (err) return send404(res);
+	        	res.writeHead(200, {'Content-Type': 'text/html'})
+	            res.write(data, 'utf8');
+	            res.end();
+	     });	
+
+    break;
+    
+    default: send404(res);
+}
+}), 
+
+send404 = function(res){
+    res.writeHead(404);
+    res.write('404');
+    res.end();
+};
+
+server.listen(3000);
+console.log("Server running at http://127.0.0.1:18080/");
+
+var io = require('socket.io').listen(server);
+
+//connection event
+io.sockets.on('connection', function(socket){
+	console.log("Connection " + socket.id + " accepted!");
+	
+	socket.on('disconnect', function(){
+		console.log("Connection " + socket.id + " terminated!");
+	});
+	
+	//receive message
+	socket.on('message', function(message){
+		console.log("Received " + message + "  -  from client  " +  socket.id);
+		//relay message
+		io.sockets.emit('chat', socket.id, message);
+	});
+	
+	
 });
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
-});
+
 
 
 
